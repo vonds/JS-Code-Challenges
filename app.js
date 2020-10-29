475c56c08f16418258c9556cf295ba9114b139c2
Vonds <37168302+vonds@users.noreply.github.com>
2020-10-29 00:03:33 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Pushing this as my first commit
+
+function rotLeft(array, step) {
+  const newArray = [...array];
+  for(let i = 1; i <= step; i++) {
+      newArray.push(newArray.shift());
+  }
+  return newArray
+}
+
+console.log(rotLeft([1, 2, 3, 4, 5], 4)) // [5, 1, 2, 3, 4]
+console.log(rotLeft([1, 2, 3, 4, 5], 2)) // [3, 4, 5, 1, 2]
