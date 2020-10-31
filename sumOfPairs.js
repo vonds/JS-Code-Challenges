8bc927c667c33d4f7b15ce2af464e6b4806a884b
Vonds <37168302+vonds@users.noreply.github.com>
2020-10-31 09:44:28 -0400

    Update sumOfPairs.js

--- sumOfPairs.js
+++ sumOfPairs.js
@@ -14,26 +14,3 @@ const sumPairs = (numbers, sum) => {
 
   console.log(sumPairs([10, 5, 2, 3, 7, 5], 10))
 
-  const languages = {
-    english: 'Welcome',
-    czech: 'Vitejte',
-    danish: 'Velkomst',
-    dutch: 'Welkom',
-    estonian: 'Tere tulemast',
-    finnish: 'Tervetuloa',
-    flemish: 'Welgekomen',
-    french: 'Bienvenue',
-    german: 'Willkommen',
-    irish: 'Failte',
-    italian: 'Benvenuto',
-    latvian: 'Gaidits',
-    lithuanian: 'Laukiamas',
-    polish: 'Witamy',
-    spanish: 'Bienvenido',
-    swedish: 'Valkommen',
-    welsh: 'Croeso'
-    }
-    
-    function greet(language) {
-      return languages[language] ? `${languages[language]}` : 'Welcome'
-    }
