3459ffe7bf49e2cfd63e321e78b4c5acbb7949d0
Vonds <37168302+vonds@users.noreply.github.com>
2020-12-12 11:09:23 -0500

    Add files via upload

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,3 +1,2 @@
 // https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 const descendingOrder = n => +[...(n.toString())].map(Number).sort((a, b) => b - a).join('')
-const descendingOrder = n => parseInt([...String(n)].sort().reverse().join(''))
