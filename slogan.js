d68a38caab14bf3f10c2b48d46142630aa7b9679
Vonds <37168302+vonds@users.noreply.github.com>
2020-12-15 07:50:20 -0500

    Add files via upload

--- slogan.js
+++ slogan.js
@@ -1,8 +1,14 @@
 // https://www.codewars.com/kata/58bf79fdc8bd4432d6000029
-function slogans(p, r, len = r.length) {
-    return len ? 
-        ( p.endsWith(r.slice(0,len)) ?  
-            1 + slogans(p,r.slice(len)) : 
-            slogans(p,r,len-1) ) : 
-        0
+function slogans(p,r) {
+    let count = 0;
+    let inSlogan = '';
+    for (let char of r) {
+      inSlogan += char;
+      if(!p.includes(inSlogan)) {
+        count++;
+        inSlogan = char;
+      }
+    }
+    if (inSlogan.length > 0) count++;
+    return count;
 }
