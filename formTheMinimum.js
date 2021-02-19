97d870d7897df56f7bddc180d51644d139ca56e3
Vonds <37168302+vonds@users.noreply.github.com>
2021-02-19 08:53:47 -0500

    Add files via upload

--- formTheMinimum.js
+++ formTheMinimum.js
@@ -1,8 +1,2 @@
-function minValue(values){
-    let str = ''
-    const sortedArr = values.sort((a, b) => a - b)
-    const set =  new Set(sortedArr)
-    set.forEach(num => str += num.toString())
-    return +str
-}
-  
+// https://www.codewars.com/kata/5ac6932b2f317b96980000ca
+const minValue = values => +(Array.from(new Set(values))).sort((a,b) => a - b).join('')
