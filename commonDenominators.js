02ce9281467fac6d6f5b9713c37c5afa6fd263e2
Vonds <37168302+vonds@users.noreply.github.com>
2021-03-26 08:10:25 -0400

    Add files via upload

--- commonDenominators.js
+++ commonDenominators.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/54d7660d2daf68c619000d95
+// https://www.codewars.com/kata/54d7660d2daf68c619000d95       https://github.com/vonds/JS-Code-Challenges/blob/main/commonDenominators.js
 const gcd = (a, b) => b ? gcd(b, a % b) : a
 const lcm = (a, b) => a * b / gcd(a, b)
 
