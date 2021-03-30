78edb9b56a1268f0a90b317adc2d6891048aa18d
Vonds <37168302+vonds@users.noreply.github.com>
2021-03-30 10:15:40 -0400

    Add files via upload

---
+++ extractTheDomainNameFromAUrl.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/514a024011ea4fb54200004b
+function domainName(url){
+    url = url.replace("https://", '')
+    url = url.replace("http://", '')
+    url = url.replace("www.", '')
+    return url.split('.')[0]
+}
