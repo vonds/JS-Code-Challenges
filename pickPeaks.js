02750f0f061a9c7652ba27e7851d920c0b637364
Vonds <37168302+vonds@users.noreply.github.com>
2021-04-03 09:53:28 -0400

    Add files via upload

---
+++ pickPeaks.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
+function pickPeaks(arr){
+    let result = { pos: [], peaks: [] }
+    for (let i = 1; i < arr.length; i++) {
+      var position
+      if (arr[i] > arr[i - 1]){
+        position = i
+      } else if (arr[i] < arr[i - 1] && position){
+        result.pos.push(position)
+        result.peaks.push(arr[position])
+        position = undefined
+      }
+    }
+    return result
+}
