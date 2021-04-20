60870a740191d1f727a06a271ef1ff6091e3eb96
Vonds <37168302+vonds@users.noreply.github.com>
2021-04-20 09:26:07 -0400

    Add files via upload

---
+++ countOnesInSegment.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/596d34df24a04ee1e3000a25
+
+const pattern = (num, cipher) => (1 << cipher) * (num >> (cipher + 1))
+
+const restOfBinary = (num, cipher) => Math.max(0, (num % (1 << cipher << 1)) - (1 << cipher) + 1)
+
+const column = (num, cipher) => pattern(num, cipher) + restOfBinary(num, cipher)
+
+const addUpTo = num => {
+  let bit = 0, total = 0
+    while ((num >> bit) > 0) total += column(num, bit++)
+    return total
+}
+
+const countOnes = (left, right) => addUpTo(right) - addUpTo(left - 1)
